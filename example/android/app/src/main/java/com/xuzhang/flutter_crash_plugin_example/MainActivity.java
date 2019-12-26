dc982ab14135b28c80d472001109a504ca0dee58
zhangxu <zhangxusy@foxmail.com>
2019-12-26 18:48:34 +0800

    Initial commit

---
+++ example/android/app/src/main/java/com/xuzhang/flutter_crash_plugin_example/MainActivity.java
@@ -0,0 +1,13 @@
+package com.xuzhang.flutter_crash_plugin_example;
+
+import android.support.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
