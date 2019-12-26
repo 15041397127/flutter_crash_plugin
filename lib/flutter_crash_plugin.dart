c6bfc9138c9ebf23558e0400a9a17fb8b4548b59
zhangxu <zhangxusy@foxmail.com>
2019-12-26 18:54:25 +0800

    .gitignore fix

--- lib/flutter_crash_plugin.dart
+++ lib/flutter_crash_plugin.dart
@@ -6,6 +6,7 @@ class FlutterCrashPlugin {
   static const MethodChannel _channel =
       const MethodChannel('flutter_crash_plugin');
 
+
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
