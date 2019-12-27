d69f0bcccfa66cca47b565a8859a86f2df450438
zhangxu <zhangxusy@foxmail.com>
2019-12-27 09:53:56 +0800

    完善iOS注册插件开发

--- ios/Classes/FlutterCrashPlugin.m
+++ ios/Classes/FlutterCrashPlugin.m
@@ -1,20 +1,48 @@
 #import "FlutterCrashPlugin.h"
-
+#import <Bugly/Bugly.h>
 @implementation FlutterCrashPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"flutter_crash_plugin"
-            binaryMessenger:[registrar messenger]];
-  FlutterCrashPlugin* instance = [[FlutterCrashPlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+    
+    //注册方法通道
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"flutter_crash_plugin"
+                                     binaryMessenger:[registrar messenger]];
+    
+    //初始化插件实例 绑定方法通道
+    FlutterCrashPlugin* instance = [[FlutterCrashPlugin alloc] init];
+    //注册方法通道回调函数
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
+    if ([@"setUp" isEqualToString:call.method]) {
+        
+        //Bugly SDK初始化方法
+        NSString *appID = call.arguments[@"app_id"];
+        
+        [Bugly startWithAppId:appID];
+        
+    }else if ([@"postException" isEqualToString:call.method]){
+        
+        // 获取Bugly数据上报所需的各个参数信息
+        NSString *mssage = call.arguments[@"crash_message"];
+        
+        NSString *detail = call.arguments[@"crash_detail"];
+        
+        NSArray *stack = [detail componentsSeparatedByString:@"\n"];
+        
+        //调用Bugly数据上报接口
+        [Bugly reportExceptionWithCategory:4 name:mssage reason:stack[0] callStack:stack extraInfo:@{} terminateApp:NO];
+        
+        
+        result(@0);
+        
+    }
+    
+    
+    else {
+        result(FlutterMethodNotImplemented);
+    }
 }
 
 @end
