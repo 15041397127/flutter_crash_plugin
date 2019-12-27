b0f49687ff86c7c09f112f7362dfd8b296fc5ca0
zhangxu <zhangxusy@foxmail.com>
2019-12-27 09:40:36 +0800

    添加方法通道 和回调方法

--- lib/flutter_crash_plugin.dart
+++ lib/flutter_crash_plugin.dart
@@ -3,8 +3,25 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class FlutterCrashPlugin {
+  
+  //初始化方法通道
   static const MethodChannel _channel =
       const MethodChannel('flutter_crash_plugin');
+  
+  
+  static void setUp(appID){
+    
+    //使用app_id进行SDK注册
+    _channel.invokeMethod("setUp",{'app_id':appID});
+    
+  }
+  
+  static void postException(error,stack){
+    
+    //将异常和堆栈报至Bugly
+    _channel.invokeMethod("postException",{'crash_message':error.toString(),'crash_detail':stack.toString()});
+    
+  }
 
 
   static Future<String> get platformVersion async {
