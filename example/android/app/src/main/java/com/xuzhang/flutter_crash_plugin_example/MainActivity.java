fabede75337646b25865bf7f81cd91ecf229261d
zhangxu <zhangxusy@foxmail.com>
2019-12-27 18:54:03 +0800

    支持适配1.9版本

--- example/android/app/src/main/java/com/xuzhang/flutter_crash_plugin_example/MainActivity.java
+++ example/android/app/src/main/java/com/xuzhang/flutter_crash_plugin_example/MainActivity.java
@@ -3,7 +3,8 @@ package com.xuzhang.flutter_crash_plugin_example;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import io.flutter.embedding.android.FlutterActivity;
+//import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.app.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
@@ -16,7 +17,7 @@ import io.flutter.plugins.GeneratedPluginRegistrant;
 //
 //}
 
-public class MainActivity extends io.flutter.app.FlutterActivity {
+public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
